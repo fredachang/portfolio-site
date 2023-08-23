13d1379e11a0b6bb33cafe5a22a8a0de5de38b55
fredachang <f.chang122@gmail.com>
2023-08-23 16:04:13 +1000

    marquee changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,9 +8,19 @@ export const Footer = () => (
     <div
       className={`w-screen h-7% flex justify-between items-center px-${space.spacingMd} border-t border-t-2 border-black z-20`}
     >
-      <div className="w-1/5">
-        <Marquee speed={30}>
-          <div className="font-mono text-sm">Designed and Developed 2023.</div>
+      <div className="w-1/4">
+        <Marquee pauseOnHover speed={30}>
+          <div className="font-mono text-sm cursor-pointer">
+            Designed and developed 2023. Click to see open-source code on
+            <a
+              href="https://github.com/fredachang/portfolio-site"
+              className="mr-2"
+              target="_blank"
+            >
+              {" "}
+              Github.
+            </a>
+          </div>
         </Marquee>
       </div>
 
