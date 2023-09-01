159ddd36dcbb2327bfbb0e8fd786d3404ff43ceb
fredachang <f.chang122@gmail.com>
2023-09-01 20:14:31 +1000

    mobile: index size update

--- src/components/project/ProjectTile.tsx
+++ src/components/project/ProjectTile.tsx
@@ -28,7 +28,7 @@ export const ProjectTile = (props: Props) => {
     setShowIndexImage(false);
   };
 
-  const staticStyle = `bg-stone-50 min-w-[100px] md:min-w-[90px] md:w-full h-full border-l border-black`;
+  const staticStyle = `min-w-[70px] md:min-w-[90px] md:w-full h-full border-l border-black`;
   const expandedStyle = `bg-${colour.sitePrimaryColour} w-[800px] h-full z-20 md:z-0 border-l border-black`;
 
   return (
