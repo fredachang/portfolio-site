ff61d636f702330c191c7bb1053ce958030a61be
fredachang <f.chang122@gmail.com>
2023-09-02 12:16:39 +1000

    updated marquee spacing on mobile

--- src/components/other/NavMarquee.tsx
+++ src/components/other/NavMarquee.tsx
@@ -35,7 +35,7 @@ export const NavMarquee = (props: Props) => {
         <div
           className={`${type.link} max-w-max flex text-start md:pl-[120px] md:mb-1`}
         >
-          <div className="mr-36">
+          <div className="mr-20 md:mr-36">
             <span className="mr-10">
               {`Currently viewing ${filteredProjectType(filteredProjects)} `}
             </span>
